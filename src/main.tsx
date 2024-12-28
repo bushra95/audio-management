97912ce9f379687fd266d57284cf6a497bcb2a9b
bushra95 <77007669+bushra95@users.noreply.github.com>
2024-12-28 12:46:13 +0300

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import { ToastProvider } from './contexts/ToastContext';
-import { AuthProvider } from './contexts/AuthContext';
 import { App } from './App';
 import './index.css';
 
 const root = document.getElementById('root');
-
-if (!root) {
-  throw new Error('Root element not found');
-}
+if (!root) throw new Error('Root element not found');
 
 ReactDOM.createRoot(root).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <ToastProvider>
-        <AuthProvider>
-          <App />
-        </AuthProvider>
-      </ToastProvider>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
